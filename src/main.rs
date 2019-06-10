38f2b4bd44707525b18b7beea1e0f766eba81af7
leo60228 <iakornfeld@gmail.com>
2019-06-10 19:21:07 -0400

    Play/pause independently of success

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@ use std::thread;
 use simple_error::SimpleError;
 
 fn run() -> Result<()> {
-    play_pause();
-
     let screenshot_thread = thread::spawn(|| screenshot().map_err(|err| SimpleError::new(
         format!("Error taking screenshot!: {}", err)
     )));
     let caption = multiline_dialog("Caption")?;
     let screenshot = screenshot_thread.join().map_err(|_| SimpleError::new("Panic taking screenshot!"))??;
 
-    play_pause();
-
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = run() {
+    play_pause();
+    let run = run();
+    play_pause();
+
+    if let Err(err) = run {
         let text = err.to_string();
         eprintln!("{}", text);
         error(&text);
