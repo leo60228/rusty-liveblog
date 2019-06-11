508b893c7249ab1ce32bc5b51556cfeee2c24a52
leo60228 <iakornfeld@gmail.com>
2019-06-11 18:48:15 -0400

    MVP

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,13 @@ use image::ImageOutputFormat;
 use notify_rust::Notification;
 use simple_error::SimpleError;
 use std::error::Error;
+use std::collections::HashMap;
 use std::io::Cursor;
 use std::process::{Command, Stdio};
 use pulldown_cmark::{html, Parser};
+use typed_builder::TypedBuilder;
 use x11::keysym;
+use serde::{Serialize, Deserialize};
 
 pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
@@ -63,11 +66,177 @@ pub fn screenshot() -> Result<Vec<u8>> {
     Ok(cursor.into_inner())
 }
 
+pub fn info(msg: &str) -> Result<()> {
+    Notification::new()
+        .summary("rusty-liveblog")
+        .body(msg)
+        .timeout(notify_rust::Timeout::Milliseconds(1000))
+        .show()?;
+
+    Ok(())
+}
+
 pub fn error(error: &str) {
     Notification::new()
         .summary("Error")
         .body(error)
         .icon("dialog-error")
+        .timeout(notify_rust::Timeout::Milliseconds(5000))
         .show()
         .expect("Failed to show error notification!");
+}
+
+pub fn get_password() -> Result<String> {
+    Ok(std::fs::read_to_string(format!("{}/password.txt", env!("CARGO_MANIFEST_DIR")))?)
+}
+
+pub fn get_token() -> Result<String> {
+    Ok(std::fs::read_to_string(format!("{}/elixire.txt", env!("CARGO_MANIFEST_DIR")))?)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum WriteFreelyResponse<T> {
+    Ok {
+        code: u16,
+        data: T,
+    },
+    Err {
+        code: u16,
+        error_msg: String,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum ElixireResponse<T> {
+    Ok(T),
+    Err {
+        error: bool,
+        message: String,
+    },
+}
+
+macro_rules! response_result {
+    ($T:ty) => {
+        impl From<ElixireResponse<$T>> for Result<$T> {
+            fn from(resp: ElixireResponse<$T>) -> Result<$T> {
+                match resp {
+                    ElixireResponse::Ok(data) => Ok(data),
+                    ElixireResponse::Err { message, .. } => Err(Box::new(SimpleError::new(message))),
+                }
+            }
+        }
+
+        impl From<WriteFreelyResponse<$T>> for Result<$T> {
+            fn from(resp: WriteFreelyResponse<$T>) -> Result<$T> {
+                match resp {
+                    WriteFreelyResponse::Ok { data, .. } => Ok(data),
+                    WriteFreelyResponse::Err { error_msg, .. } => Err(Box::new(SimpleError::new(error_msg))),
+                }
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Login {
+    pub access_token: String,
+    pub user: User,
+}
+
+response_result!(Login);
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct User {
+    pub username: String,
+    pub email: String,
+    pub created: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum Font {
+    Sans,
+    Serif,
+    Wrap,
+    Mono,
+    Code,
+}
+
+#[derive(Serialize, Deserialize, Debug, TypedBuilder)]
+pub struct NewPost {
+    pub body: String,
+    #[builder(default)]
+    pub title: Option<String>,
+    #[builder(default)]
+    pub font: Option<Font>,
+    #[builder(default)]
+    pub lang: Option<String>,
+    #[builder(default)]
+    pub rtl: Option<bool>,
+    #[builder(default)]
+    pub created: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Post {
+    pub id: String,
+    pub slug: Option<String>,
+    pub appearance: String,
+    pub language: String,
+    pub rtl: bool,
+    pub created: String,
+    pub updated: String,
+    pub title: String,
+    pub body: String,
+    pub tags: Vec<String>,
+    pub views: usize,
+}
+
+response_result!(Post);
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Image {
+    pub url: String,
+    pub shortname: String,
+    pub delete_url: String,
+}
+
+response_result!(Image);
+
+pub fn authenticate(username: &str, password: &str) -> Result<Login> {
+    let mut params = HashMap::new();
+    params.insert("alias", username);
+    params.insert("pass", password);
+
+    reqwest::Client::new().post("https://blog.leo60228.space/api/auth/login")
+        .json(&params)
+        .send()?
+        .json::<WriteFreelyResponse<Login>>()?
+        .into()
+}
+
+pub fn post(token: &str, post: &NewPost) -> Result<Post> {
+     reqwest::Client::new().post("https://blog.leo60228.space/api/collections/leo60228/posts")
+        .header("Authorization", format!("Token {}", token))
+        .json(post)
+        .send()?
+        .json::<WriteFreelyResponse<Post>>()?
+        .into()
+}
+
+pub fn upload(token: &str, png: Vec<u8>) -> Result<Image> {
+    let part = reqwest::multipart::Part::bytes(png)
+        .file_name("image.png")
+        .mime_str("image/png")?;
+    let form = reqwest::multipart::Form::new()
+        .part("f", part);
+
+    reqwest::Client::new().post("https://elixire.leo60228.space/api/upload?admin")
+        .multipart(form)
+        .header("Authorization", token)
+        .send()?
+        .json::<ElixireResponse<Image>>()?
+        .into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
 use rusty_liveblog::*;
 use std::thread;
 use std::time::Duration;
-use simple_error::SimpleError;
+use finally_block::finally;
 
 fn run() -> Result<()> {
-    let screenshot_thread = thread::spawn(|| screenshot().map_err(|err| SimpleError::new(
-        format!("Error taking screenshot!: {}", err)
-    )));
+    thread::sleep(Duration::from_millis(500));
+    play_pause();
+
+    let _unpause = finally(|| {
+        thread::sleep(Duration::from_millis(500));
+        play_pause();
+    });
+
+    let screenshot = screenshot()?;
     let caption = multiline_dialog("Caption")?;
-    let screenshot = screenshot_thread.join().map_err(|_| SimpleError::new("Panic taking screenshot!"))??;
+
+    let image = upload(&get_token()?, screenshot)?;
+
+    let login = authenticate("leo60228", &get_password()?)?;
+    let new_post = NewPost::builder()
+        .body(format!("![Screenshot]({})\n\n{}", image.url, caption))
+        .build();
+    post(&login.access_token, &new_post)?;
+
+    info("Posted!")?;
 
     Ok(())
 }
 
 fn main() {
-    thread::sleep(Duration::from_millis(500));
-    play_pause();
-    let run = run();
-    thread::sleep(Duration::from_millis(500));
-    play_pause();
-
-    if let Err(err) = run {
+    if let Err(err) = run() {
         let text = err.to_string();
         eprintln!("{}", text);
         error(&text);
